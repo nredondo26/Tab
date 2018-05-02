b179d5edd3026925d2a60fef0eb7443df00e64b4
Nelson Enrique Redondo Barros <30601611+nredondo26@users.noreply.github.com>
2018-05-02 12:13:01 -0500

    Initial commit

---
+++ app/src/main/java/nredondo26/nestnet/tab/Activities/MainActivity.java
@@ -0,0 +1,49 @@
+package nredondo26.nestnet.tab.Activities;
+
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+import nredondo26.nestnet.tab.Adacter.PagerAdapter;
+import nredondo26.nestnet.tab.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText("Dispositivos"));
+        tabLayout.addTab(tabLayout.newTab().setText("Reportes"));
+        tabLayout.addTab(tabLayout.newTab().setText("Ajustes"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final ViewPager viewPager =  findViewById(R.id.pager);
+        final PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+    }
+
+}
---
+++ app/src/main/java/nredondo26/nestnet/tab/Fragment/TabFragment2.java
@@ -0,0 +1,19 @@
+package nredondo26.nestnet.tab.Fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import nredondo26.nestnet.tab.R;
+
+public class TabFragment2 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_tab_fragment2, container, false);
+    }
+}
